b7c679984d69d519bfd74e83f4bbcb9ddd438cbd
David Quinlan <dquinlan07@gmail.com>
2016-08-11 18:09:38 -0500

    changed login

--- public/assets/js/functions.js
+++ public/assets/js/functions.js
@@ -1,14 +1,14 @@
 $(document).ready(function(){
-  $('.modal-trigger').leanModal(
-    {
-        dismissible: true, // Modal can be dismissed by clicking outside of the modal
-        opacity: 0.5, // Opacity of modal background
-        in_duration: 300, // Transition in duration
-        out_duration: 300 // Transition out duration// Ending top style attribute
-      }
+   $('.modal-trigger').leanModal({
+      dismissible: true, // Modal can be dismissed by clicking outside of the modal
+      opacity: .5, // Opacity of modal background
+      in_duration: 300, // Transition in duration
+      out_duration: 200, // Transition out duration
+    }
   );
-  $(".button-collapse").sideNav();
+
   var currentURL = window.location.origin;
+
   $('#signUp').on('click', function(){
     var newUser = {
       firstName: $('#first_name').val().trim(),
@@ -19,24 +19,23 @@ $(document).ready(function(){
       email: $('#email').val().trim()
     };
     if(newUser.password === newUser.reenterpassword){ //added this
-    $.post(currentURL + "/login/new", newUser ,function(err){
-      console.log(err);
-      if (err == "alert"){
-        alert("That username is taken. Please try another.");
-      }
-      else {
-        window.location.reload();
-      }
-    });
-  }
-  else {
-    alert("Please make sure you password matches.");
-  } //added this 
+      $.post(currentURL + "/login/new", newUser ,function(err){
+        console.log(err);
+        if (err == "alert"){
+          alert("That username is taken. Please try another.");
+        }
+        else {
+          window.location.reload();
+        }
+      });
+    }
+    else {
+      alert("Please make sure you password matches.");
+    }
     return false;
   });
 
  $('#signInSubmit').on('click', function(){
-    function collectInfo(){
       var userInfo = {
         username: $('#username').val().trim(),
         password: $('#password').val().trim()
@@ -46,12 +45,11 @@ $(document).ready(function(){
         console.log(data);
       });
       return false;
-    }
-    formValidate();
+    
   }); //closes out submit button on click 
 
 
-}); //closes out document.ready 
+ });//closes out document.ready 
 
 
 
